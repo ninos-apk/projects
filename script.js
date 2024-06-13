66c33b2efa5bad680a4e005f02719ae37a1d4eac
Ninos Oshana <ninos-oshana@live.com>
2024-06-13 16:24:55 +0200

    designing the page

--- script.js
+++ script.js
@@ -1,10 +1,62 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const toggleSwitch = document.getElementById('mode-toggle');
+const navbarHTML = `
+<ul>
+    <li class="list">
+        <a href="index.html">
+            <span class="icon home">
+                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><path d="M80 212v236a16 16 0 0016 16h96V328a24 24 0 0124-24h80a24 24 0 0124 24v136h96a16 16 0 0016-16V212" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path d="M480 256L266.89 52c-5-5.28-16.69-5.34-21.78 0L32 256M400 179V64h-48v69" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/></svg>
+            </span>
+            <span class="text text-home">Home</span>
+        </a>
+    </li>
+    <li class="list">
+        <a href="projects.html">
+            <span class="icon projects">
+                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><rect x="64" y="64" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="216" y="64" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="368" y="64" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="64" y="216" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="216" y="216" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="368" y="216" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="64" y="368" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="216" y="368" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/><rect x="368" y="368" width="80" height="80" rx="40" ry="40" fill="none" stroke="currentColor" stroke-miterlimit="10" stroke-width="32"/></svg>
+            </span>
+            <span class="text text-projects">Projects</span>
+        </a>
+    </li>
+    <li class="list">
+        <a href="about.html">
+            <span class="icon about">
+                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><rect x="96" y="48" width="320" height="416" rx="48" ry="48" fill="none" stroke="currentColor" stroke-linejoin="round" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M176 128h160M176 208h160M176 288h80"/></svg>
+            </span>
+            <span class="text text-about">About</span>
+        </a>
+    </li>
+    <li class="list">
+        <a href="contact.html">
+            <span class="icon contact">
+                <svg xmlns="http://www.w3.org/2000/svg" class="ionicon" viewBox="0 0 512 512"><circle cx="128" cy="256" r="48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><circle cx="384" cy="112" r="48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><circle cx="384" cy="400" r="48" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32"/><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="32" d="M169.83 279.53l172.34 96.94M342.17 135.53l-172.34 96.94"/></svg>
+            </span>
+            <span class="text text-contact">Contact</span>
+        </a>
+    </li>
+    <li class="list list-switch">
+        <label class="switch">
+            <input id="mode-toggle" type="checkbox">
+            <span class="slider round"></span>
+        </label>
+    </li>
+</ul>
 
+`
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('body').style.opacity = 1
+    // Select the placeholder element
+    const navPlaceholder = document.getElementById('nav-placeholder');
+    navPlaceholder.innerHTML = navbarHTML;
+
+    const toggleSwitch = document.getElementById('mode-toggle');
     // Check for saved mode in localStorage
     if (localStorage.getItem('dark-mode') === 'enabled') {
         document.body.classList.add('dark-mode');
         toggleSwitch.checked = true;
+        changeColor('white');
+    }else{
+        toggleSwitch.checked = false;
+        changeColor('black');
     }
 
     toggleSwitch.addEventListener('change', function () {
@@ -18,6 +70,7 @@ document.addEventListener('DOMContentLoaded', function () {
             changeColor('black');
         }
     });
+
 });
 
 changeColor = (color) =>{
@@ -38,6 +91,4 @@ svgs.forEach(svg => {
         });
     }
 });
-
 }
-changeColor('black');
